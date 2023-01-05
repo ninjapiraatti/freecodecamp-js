a8954bffe292d1232816f934b1eff4af54597df6
ninjapiraatti <tuoppil@gmail.com>
2023-01-05 15:25:34 +0200

    Add wip arguments-optional

---
+++ arguments-optional.js
@@ -0,0 +1,30 @@
+/*
+Create a function that sums two arguments together. If only one argument
+is provided, then return a function that expects one argument and returns the sum.
+
+For example,
+
+addTogether(2, 3) should return 5, and addTogether(2)
+
+should return a function.
+
+Calling this returned function with a single argument will then return the sum:
+
+var sumTwoAnd = addTogether(2);
+sumTwoAnd(3) returns 5.
+
+If either argument isn't a valid number, return undefined.
+*/
+
+function addTogether(...args) {
+	sum = args[0] && args[1] ? args[0] + args[1] : 0
+	return sum
+}
+
+console.log(addTogether(2,3))
+console.log(addTogether(30)(23))
+
+/*
+
+
+*/
